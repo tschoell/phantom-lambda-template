c90af7577bc6f79261e02eb1eb7933b90d090e72
tschoell <tom@dumpling.us>
2019-01-05 20:23:33 -0800

    Test

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,5 +1,14 @@
-console.log('start phantom');
-setTimeout(function() {
-    console.log('Boo from phantom');
-    phantom.exit();
-}, 1000);
+// console.log('start phantom');
+// setTimeout(function() {
+//     console.log('Boo from phantom');
+//     phantom.exit();
+// }, 1000);
+
+var webPage = require('webpage');
+var page = webPage.create();
+
+page.open('http://phantomjs.org', function (status) {
+  var content = page.content;
+  console.log('Content: ' + content);
+  phantom.exit();
+});
